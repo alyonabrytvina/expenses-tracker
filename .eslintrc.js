67d195cc6b4bbf5fabfe8c0cd0a3be362efe7ac7
alonabrytvina <alyona.ivanova.kh@gmail.com>
2023-11-24 01:09:10 +0100

    implement packages, extend eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,11 @@ module.exports = {
   parserOptions: {
     project: ['./tsconfig.json'],
   },
-  rules: {},
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+    'no-spaced-func': 'off',
+  },
 };
